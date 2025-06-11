fad8144cb3f2c2ab5479be3837a66b7ea6f3b7c7
Joey G <joegiusto@outlook.com>
2025-06-11 04:03:56 -0400

    Now using the Tray for electron

--- electron/main.js
+++ electron/main.js
@@ -1,14 +1,63 @@
-const { app, BrowserWindow } = require('electron');
+import { app, BrowserWindow, Menu, Tray } from 'electron';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+let __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+let mainWindow;
+let tray;
 
 function createWindow() {
-  const win = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 400,
-    height: 300
+    height: 300,
+    icon: path.join(__dirname, 'icon.png'), // Set window icon
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
   });
 
-  win.setMenuBarVisibility(false);
+  mainWindow.setMenuBarVisibility(false);
+  mainWindow.loadFile('./index.html');
 
-  win.loadFile('./index.html');
+  // Intercept close and hide instead (minimize to tray)
+  mainWindow.on('close', (event) => {
+    if (!app.isQuiting) {
+      event.preventDefault();
+      mainWindow.hide();
+    }
+    return false;
+  });
+
+  // Create system tray icon
+  tray = new Tray(path.join(__dirname, 'icon.png'));
+
+  const trayMenu = Menu.buildFromTemplate([
+    {
+      label: 'Show App',
+      click: () => {
+        mainWindow.show();
+        mainWindow.focus();
+      }
+    },
+    {
+      label: 'Quit',
+      click: () => {
+        app.isQuiting = true;
+        tray.destroy();
+        app.quit();
+      }
+    }
+  ]);
+
+  tray.setToolTip('My Electron App');
+  tray.setContextMenu(trayMenu);
+
+  tray.on('double-click', () => {
+    mainWindow.show();
+    mainWindow.focus();
+  });
 }
 
 app.whenReady().then(createWindow);
@@ -16,4 +65,12 @@ app.whenReady().then(createWindow);
 app.on('window-all-closed', () => {
   // Quit the app when all windows are closed (except on macOS)
   if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  } else {
+    mainWindow.show();
+  }
 });
